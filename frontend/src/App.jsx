e502ce819a3c6b8fbc55ce9af34037b0d670cba6
NGC-337-2 <nikhilap9699@gmail.com>
2026-06-23 09:00:32 +0530

    frontend: setup main entrypoint and configure react-router routes

---
+++ frontend/src/App.jsx
@@ -0,0 +1,41 @@
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { AuthProvider } from "./context/AuthContext";
+import ProtectedRoute from "./components/ProtectedRoute";
+import NavBar from "./components/NavBar";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import Search from "./pages/Search";
+import History from "./pages/History";
+
+export default function App() {
+  return (
+    <AuthProvider>
+      <BrowserRouter>
+        <NavBar />
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route
+            path="/search"
+            element={
+              <ProtectedRoute>
+                <Search />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/history"
+            element={
+              <ProtectedRoute>
+                <History />
+              </ProtectedRoute>
+            }
+          />
+          {/* Default redirect */}
+          <Route path="/" element={<Navigate to="/search" replace />} />
+          <Route path="*" element={<Navigate to="/search" replace />} />
+        </Routes>
+      </BrowserRouter>
+    </AuthProvider>
+  );
+}
